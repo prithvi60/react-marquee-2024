058bfdf4da3a381fdf32e8b1ffeadf76c7ba64ce
Prithvi <prithvin19@gmail.com>
2024-07-23 18:08:09 +0530

    v1

--- components/Marquee.jsx
+++ components/Marquee.jsx
@@ -1,6 +1,6 @@
 "use client";
-import { CloneList } from "@/helper/CloneArray";
-import React, { useRef, useState, useEffect } from "react";
+// import { CloneList } from "@/helper/CloneArray";
+import React, { useRef, useState, useEffect, useMemo } from "react";
 
 // const duplicates = useMemo(() => {
 //   return autofill && elements.length <= 8 ? CloneList(elements) : elements;
@@ -14,28 +14,30 @@ const Marquee = ({
   children,
 }) => {
   const containerRef = useRef(null);
-  const scrollPositionRef = useRef(0);
 
   const [mouseDown, setMouseDown] = useState(false);
   const [clientX, setClientX] = useState(0);
   const [scrollLeft, setScrollLeft] = useState(0);
-  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const styles = ` flex w-full p-5 gap-3.5 border-2 group snap-x snap-mandatory overflow-x-auto custom  ${
-    mouseDown
-      ? "cursor-grabbing scroll-smooth transition-transform duration-500 ease-linear"
-      : "cursor-pointer scroll-auto"
-  }`;
+  const styles = useMemo(
+    () =>
+      `flex w-full p-5 gap-3.5 border-2 group snap-x snap-mandatory overflow-x-auto custom ${
+        mouseDown
+          ? "cursor-grabbing scroll-smooth transition-transform duration-500 ease-linear"
+          : "cursor-pointer scroll-auto"
+      }`,
+    [mouseDown]
+  );
+
   // MouseDown event was used to collect client X value and stored in the ClientX state
   const handleDrag = (e) => {
     setMouseDown(true);
-    setClientX((prev) => e.pageX);
-    setScrollLeft((prev) => containerRef.current.scrollLeft);
+    setClientX(() => e.pageX);
+    setScrollLeft(() => containerRef.current.scrollLeft);
   };
 
   const handleMove = (e) => {
     e.preventDefault();
-    //
     if (!mouseDown) {
       return;
     }
@@ -51,40 +53,37 @@ const Marquee = ({
     if (containerRef.current.scrollLeft === 0) {
       // if the scroll is at the beginning,scroll to the end
       containerRef.current.scrollLeft = scrollbarWidth - 2 * containerWidth;
-      // console.log("left end");
     } else if (
       containerRef.current.scrollLeft ===
       scrollbarWidth - containerWidth
     ) {
       // if the scroll is at the end,scroll to the beginning
       containerRef.current.scrollLeft = containerWidth;
-      // console.log("right end");
     }
   };
+
+  const renderMarquee = (isHidden) => (
+    <div
+      className="animate-marquee flex snap-start items-center gap-3.5 select-none group-hover:paused transition-transform duration-500 ease-linear"
+      aria-hidden={isHidden}
+    >
+      {children}
+    </div>
+  );
 
   return (
     <div
       ref={containerRef}
       className={styles}
       // functional events
+      onScroll={(e) => handleInfiniteScroll(e)}
       onMouseDown={(e) => handleDrag(e)}
       onMouseMove={(e) => handleMove(e)}
       onMouseLeave={() => setMouseDown(false)}
       onMouseUp={() => setMouseDown(false)}
-      onScroll={(e) => handleInfiniteScroll(e)}
     >
-      {/* Duplication of code for Marquee */}
-      <div
-        className={`animate-marquee flex snap-start items-center gap-3.5 select-none group-hover:paused transition-transform duration-500 ease-linear`}
-      >
-        {children}
-      </div>
-      <div
-        className={`animate-marquee flex snap-start items-center gap-3.5 select-none group-hover:paused transition-transform duration-500 ease-linear`}
-        aria-hidden={true}
-      >
-        {children}
-      </div>
+      {renderMarquee(false)}
+      {renderMarquee(true)}
     </div>
   );
 };
